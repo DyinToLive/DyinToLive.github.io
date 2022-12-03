b4e7ae20fb3ed6f676801a771ac97f265a77e964
DyinToLive <88452454+DyinToLive@users.noreply.github.com>
2022-12-03 13:39:49 -0700

    song radar analytics chart completed

--- js/acquisitions.js
+++ js/acquisitions.js
@@ -27,3 +27,101 @@ import { Chart } from 'chart.js/auto'
         }
     );
 })();
+
+const radarChart = {
+    type: 'radar',
+    data: analytics,
+    options: {
+        elements: {
+            line: {
+                borderWidth: 3
+            }
+        }
+    },
+};
+const analytics = {
+    labels: [
+        'energy',
+        'danceability',
+        'liveness',
+        'valence',
+        'acousticness',
+        'speechiness'
+    ],
+    datasets: [{
+        label: 'My First Dataset',
+        data: [59, 76, 18, 81, 8, 23],
+        fill: true,
+        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+        borderColor: 'rgb(255, 99, 132)',
+        pointBackgroundColor: 'rgb(255, 99, 132)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgb(255, 99, 132)'
+    }, {
+        label: 'My Second Dataset',
+        data: [73, 62, 14, 37, 2, 11],
+        fill: true,
+        backgroundColor: 'rgba(54, 162, 235, 0.2)',
+        borderColor: 'rgb(54, 162, 235)',
+        pointBackgroundColor: 'rgb(54, 162, 235)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgb(54, 162, 235)'
+    }, {
+        label: 'My Third Dataset',
+        data: [74, 65, 24, 61, 5, 23],
+        fill: true,
+        backgroundColor: 'rgba(54, 162, 235, 0.2)',
+        borderColor: 'rgb(54, 162, 235)',
+        pointBackgroundColor: 'rgb(54, 162, 235)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgb(54, 162, 235)'
+    },
+    {
+        label: 'My Fourth Dataset',
+        data: [60, 66, 36, 29, 5, 4],
+        fill: true,
+        backgroundColor: 'rgba(54, 162, 235, 0.2)',
+        borderColor: 'rgb(54, 162, 235)',
+        pointBackgroundColor: 'rgb(54, 162, 235)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgb(54, 162, 235)'
+    },
+    {
+        label: 'My Fifth Dataset',
+        data: [60, 78, 25, 36, 0, 5],
+        fill: true,
+        backgroundColor: 'rgba(54, 162, 235, 0.2)',
+        borderColor: 'rgb(54, 162, 235)',
+        pointBackgroundColor: 'rgb(54, 162, 235)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgb(54, 162, 235)'
+    }]
+};
+
+const ctx = document.getElementById('songChart');
+
+new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+export { radarChart };
--- js/assign2.js
+++ js/assign2.js
@@ -1,4 +1,3 @@
-
 document.addEventListener("DOMContentLoaded", function () {
     /* url of song api --- https versions hopefully a little later this semester */
     const api = 'https://www.randyconnolly.com/funwebdev/3rd/api/music/songs-nested.php';
@@ -27,6 +26,7 @@ document.addEventListener("DOMContentLoaded", function () {
             }).catch(err => {console.log('err=' + err)});
     }
     else {
+
         const artists = JSON.parse(content);
         const genres = JSON.parse(content2);
         const songs = JSON.parse(jsonData);
@@ -408,6 +408,8 @@ document.addEventListener("DOMContentLoaded", function () {
                     e.preventDefault();
                     //e.stopPropagation();
                     populateSongView(single);
+
+                    populateChart(single);
                 }
             });
         }
@@ -506,40 +508,73 @@ document.addEventListener("DOMContentLoaded", function () {
         tableRow.appendChild(year);
         tableRow.appendChild(duration);
         singleSongBody.appendChild(tableRow);
-
-        const data = {
-            labels: [
-                'Eating',
-                'Drinking',
-                'Sleeping',
-                'Designing',
-                'Coding',
-                'Cycling',
-                'Running'
-            ],
-            datasets: [{
-                label: 'My First Dataset',
-                data: [65, 59, 90, 81, 56, 55, 40],
-                fill: true,
-                backgroundColor: 'rgba(255, 99, 132, 0.2)',
-                borderColor: 'rgb(255, 99, 132)',
-                pointBackgroundColor: 'rgb(255, 99, 132)',
-                pointBorderColor: '#fff',
-                pointHoverBackgroundColor: '#fff',
-                pointHoverBorderColor: 'rgb(255, 99, 132)'
-            }, {
-                label: 'My Second Dataset',
-                data: [28, 48, 40, 19, 96, 27, 100],
-                fill: true,
-                backgroundColor: 'rgba(54, 162, 235, 0.2)',
-                borderColor: 'rgb(54, 162, 235)',
-                pointBackgroundColor: 'rgb(54, 162, 235)',
-                pointBorderColor: '#fff',
-                pointHoverBackgroundColor: '#fff',
-                pointHoverBorderColor: 'rgb(54, 162, 235)'
-            }]
-        };
-
+        //end of populate song
     }
-
+    function populateChart(single) {
+        const ctx = document.getElementById('radarChart');
+        //if chart exists, destroy it before building new chart
+        if (this.myChart) {
+            this.myChart.destroy();
+            this.myChart = new Chart(ctx, {
+                type: 'radar',
+                data: {
+                    labels: ['Energy',
+                        'Danceability',
+                        'Liveness',
+                        'Valence',
+                        'Acousticness',
+                        'Speechiness'],
+                    datasets: [{
+                        label: 'Song Analytics',
+                        data: [single.analytics.energy, single.analytics.danceability, single.analytics.liveness,
+                        single.analytics.valence, single.analytics.acousticness, single.analytics.speechiness],
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
+                    }
+                }
+            });
+            
+        }
+        else {
+            //build chart if doesnt exist
+        this.myChart = new Chart(ctx, {
+            type: 'radar',
+            data: {
+                labels: ['Energy',
+                    'Danceability',
+                    'Liveness',
+                    'Valence',
+                    'Acousticness',
+                    'Speechiness'],
+                datasets: [{
+                    label: 'Song Analytics',
+                    data: [single.analytics.energy, single.analytics.danceability, single.analytics.liveness,
+                    single.analytics.valence, single.analytics.acousticness, single.analytics.speechiness],
+                    borderWidth: 1,
+                    fill: true,
+                    backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                    borderColor: 'rgb(54, 162, 235)',
+                    pointBackgroundColor: 'rgb(54, 162, 235)',
+                    pointBorderColor: '#fff',
+                    pointHoverBackgroundColor: '#fff',
+                    pointHoverBorderColor: 'rgb(54, 162, 235)'
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+        }
+        
+    }
 });
